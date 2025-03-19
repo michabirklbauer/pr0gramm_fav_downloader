a4f3f43179d1ca8f52bee690b087dafe6a3975c1
Micha Birklbauer <micha.birklbauer@gmail.com>
2025-03-19 19:57:30 +0100

    add dummy pytest

---
+++ tests/tests.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# 2025 (c) Micha Johannes Birklbauer
+# https://github.com/michabirklbauer/
+# micha.birklbauer@gmail.com
+
+def test1():
+    from download import download_item, download_collection
+    assert 0 == 0
